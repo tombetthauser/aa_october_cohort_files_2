50d26b1adf58d8245e662f83731e5d71377b3e02
tombetthauser <tombetthauser@gmail.com>
2019-12-08 23:07:06 -0800

    Deletes practice test

--- assessments/A03/a03-prep-master/practice_1/assoc-solution/spec/factories/watch_list_items.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :watch_list_item do
-    
-  end
-end
--- assessments/A03/a03-prep-master/practice_1/assoc-solution/spec/models/exchange_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Exchange, type: :model do
-  subject(:nasdaq) { Exchange.find_by_name("NASDAQ") }
-
-  it "has a company" do
-    expect(nasdaq.companies.first.name).to eq("Google")
-  end
-end
