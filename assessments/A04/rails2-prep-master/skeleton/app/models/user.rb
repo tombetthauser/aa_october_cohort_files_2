5dd2e6be1d3c4a7c7ce093c9ca91c693f9379c67
tombetthauser <tombetthauser@gmail.com>
2019-12-22 14:28:40 -0800

    Finishes A04p vid notes and starts A04p pass#1

---
+++ assessments/A04/rails2-prep-master/skeleton/app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+
+    validates :username, :password_digest, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    attr_reader :password
+
+    after_initialize :ensure_session_token
+
+
+
+    # FiVer? = ResEn --> Find, Verify, Change, Reset, Ensure
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        user && user.is_password?(password) ? user : nil
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token
+        self.session_token = SecureRandom.urlsafe_base64(16)
+        
+    end
+
+    def ensure_session_token
+    end
+end
