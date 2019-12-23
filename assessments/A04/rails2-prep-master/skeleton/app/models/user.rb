ef2c1e539ada7613f87aa8e3a5eadd523061ab0d
tombetthauser <tombetthauser@gmail.com>
2019-12-22 21:20:01 -0800

    Adds password cracking method for fun

--- assessments/A04/rails2-prep-master/skeleton/app/models/user.rb
+++ assessments/A04/rails2-prep-master/skeleton/app/models/user.rb
@@ -1,15 +1,13 @@
 class User < ApplicationRecord
 
-    validates :username, :password_digest, presence: true
+    validates :username, :session_token, :password_digest, presence: true
     validates :password, length: { minimum: 6, allow_nil: true }
 
     attr_reader :password
-
     after_initialize :ensure_session_token
 
-
-
-    # FiVer? = ResEn --> Find, Verify, Change, Reset, Ensure
+    has_many :comments
+    has_many :links
 
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
@@ -17,18 +15,45 @@ class User < ApplicationRecord
     end
 
     def is_password?(password)
-        BCrypt::Password.new(self.password_digest).is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
     end
 
     def password=(password)
+        @password = password
         self.password_digest = BCrypt::Password.create(password)
     end
 
     def reset_session_token
         self.session_token = SecureRandom.urlsafe_base64(16)
-        
     end
 
     def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64(16)
     end
+
+end
+
+# ~9min almost no peeks needed
+# ~6.5min no peeking
+
+
+def random_password(max_length)
+    # random_length = rand(max_length)
+    # SecureRandom.urlsafe_base64(random_length)
+    SecureRandom.urlsafe_base64(max_length)
+end
+
+def crack(correct_password)
+    
+    guessed_password = rand(10)
+    guess_count = 1
+    
+    until correct_password == guessed_password
+        guessed_password = rand(10)
+        guess_count += 1
+        puts guess_count
+    end
+
+    puts "Password cracked in #{guess_count} random tries."
+    { guess_count: guess_count, original_password: correct_password, cracked_password: guessed_password }
 end
