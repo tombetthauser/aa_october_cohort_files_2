b5c7433b17fedfea08fcdebc5003cf5836d6fb0b
tombetthauser <tombetthauser@gmail.com>
2019-12-23 11:27:31 -0800

    Migrated comments in A04p

--- assessments/A04/rails2-prep-master/skeleton/db/schema.rb
+++ assessments/A04/rails2-prep-master/skeleton/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_185506) do
+ActiveRecord::Schema.define(version: 2019_12_23_190757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "user_id", null: false
+    t.integer "link_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_comments_on_link_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "links", force: :cascade do |t|
     t.string "title", null: false
