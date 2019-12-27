983f7ccf4dc9e1c2943fdaefe2d29fa04c6f0505
tombetthauser <tombetthauser@gmail.com>
2019-12-27 10:49:31 -0800

    Completes comment and link models passing all specs 10:48am

--- assessments/A04/Tom_4thPass_rails2-prep-master/skeleton/app/models/comment.rb
+++ assessments/A04/Tom_4thPass_rails2-prep-master/skeleton/app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+
+    validates :body, :user, :link, presence: true
+
+    belongs_to :user
+    belongs_to :link
+
 end
--- assessments/A04/Tom_4thPass_rails2-prep-master/skeleton/app/models/link.rb
+++ assessments/A04/Tom_4thPass_rails2-prep-master/skeleton/app/models/link.rb
@@ -1,2 +1,8 @@
 class Link < ApplicationRecord
+
+    validates :title, :url, :user, presence: true
+
+    belongs_to :user
+    has_many :comments
+
 end
