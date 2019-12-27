b255d752f059e203e98d243e7fc70f0c14d8ad60
tombetthauser <tombetthauser@gmail.com>
2019-12-27 11:24:44 -0800

    Completes application_controller comments_controller and routes with a lot of notes 11:23am

--- assessments/A04/Tom_4thPass_rails2-prep-master/skeleton/config/routes.rb
+++ assessments/A04/Tom_4thPass_rails2-prep-master/skeleton/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   get 'users/hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    resource :session, only: [:new, :create, :destroy]
+
+    resources :users, only: [:new, :create]
+    resources :comments, only: [:destroy]
+    resources :links do
+      resources :comments, only: [:create]
+    end
+
 end
