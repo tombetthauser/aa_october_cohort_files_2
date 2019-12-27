06334e339b1803a23403171c11f11cf5331e31b7
tombetthauser <tombetthauser@gmail.com>
2019-12-27 11:55:09 -0800

    Completes all controllers with extensive use of notes 11:55am

--- assessments/A04/Tom_4thPass_rails2-prep-master/skeleton/app/controllers/sessions_controller.rb
+++ assessments/A04/Tom_4thPass_rails2-prep-master/skeleton/app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+    
+    def create
+        user = User.find_by_credentials(
+            params[:user][:username],            
+            params[:user][:password]                
+        )
+
+        if user
+            login(user)
+            redirect_to links_url
+        else
+            flash.now[:errors] = ["Invalid username and/or password."]
+            render :new
+        end
+    end
+    
+    def destroy
+        logout
+        redirect_to new_session_url
+    end
+    
+    def new
+        # render :new
+    end
+
 end
