72d8a31e74e3e8684fdaee40876e7828a3244528
tombetthauser <tombetthauser@gmail.com>
2019-12-27 09:42:32 -0800

    Organizes screenshots and finishes A04p

---
+++ assessments/A04/Toms_First_Pass_rails2-prep-master/skeleton/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :session, only: [:new, :create, :destroy]
+
+  resources :users, only: [:new, :create]
+  resources :comments, only: [:destroy]
+  resources :links do
+    resources :comments, only: [:create]
+  end
+
+
+end
