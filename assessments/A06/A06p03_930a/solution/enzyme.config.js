d334e200ef937bc45a772491b6467c3385d7bace
tombetthauser <tombetthauser@gmail.com>
2020-01-26 22:08:43 -0800

    Compresses Finished A06p03

--- assessments/A06/A06p03_930a/solution/enzyme.config.js
+++
@@ -1,7 +0,0 @@
-// setup file
-import Enzyme, { configure, shallow, mount, render } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
-export { shallow, mount, render };
-export default Enzyme;
