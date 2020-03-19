cd5cc84fddc9662c17cb93f9bf4103c50d1ce08b
tombetthauser <tombetthauser@gmail.com>
2020-03-19 16:56:09 -0700

    Test for leet code passed

--- classworks/test.py
+++ classworks/test.py
@@ -1,1 +1,21 @@
-print("hello")
+# def test(name):
+#   return ("hello " + name)
+
+# print(test("tom"))
+
+
+def wordBreak(s, wordDict):
+    table = [False] * (len(s) + 1)
+    table[0] = True
+    
+    for i in range(0, len(table)):
+      if (table[i] == True):
+        for j in range(i + 1, len(table)):
+          word = s[i:j]
+          if word in wordDict:
+            table[j] = True
+            
+    return table[-1]
+
+n = wordBreak("catdogcat", ["cat", "dog"])
+print(n)
